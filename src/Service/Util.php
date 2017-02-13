c4f1dfb4c6bb2b608403177192fea96f7cf5a8d5
Aarne Welschlau <aarne.welschlau@ratepay.com>
2017-02-13 17:55:01 +0100

    Adding new installment builder

--- src/Service/Util.php
+++ src/Service/Util.php
@@ -59,4 +59,81 @@
             return $stringFormatted;
         }
 
+        /**
+         * Replaces entered string by defined identifiers
+         *
+         * @param string $template
+         * @param array $dataSet
+         * @param string|null $key
+         * @param string|null $value
+         * @return string
+         */
+        public static function templateReplacer($template, $dataSet = [], $key = null, $value = null)
+        {
+            $tpl = $template;
+
+            if (is_null($key)) {
+                foreach ($dataSet as $key => $value) {
+                    $tpl = self::templateReplacer($tpl, $dataSet, $key, $value);
+                }
+            } else {
+                $tpl = str_replace("{{ " . $key . " }}", $value, $tpl);
+            }
+
+            return $tpl;
+        }
+
+        /**
+         * Replaces entered string by defined identifiers
+         *
+         * @param $template
+         * @param $loopDataSet
+         * @return string
+         */
+        public static function templateLooper($template, $loopDataSet)
+        {
+            $tpl = $template;
+
+            $loop = "loop";
+
+            if (strstr($tpl, "@" . $loop)) {
+                $splitTpl = self::splitStringByCommand($tpl, "@" . $loop, "@end" . $loop);
+
+                $tplLoopedContent = "";
+                foreach ($loopDataSet as $key => $loopValue) {
+                    if (is_array($loopValue)) {
+                        foreach ($loopValue as $value) {
+                            $tplLoopedContent .= self::templateReplacer($splitTpl['innerText'], [], $key, $value);
+                        }
+                    }
+                }
+
+                $tpl = $splitTpl['preText'] . $tplLoopedContent . $splitTpl['postText'];
+            }
+
+            return $tpl;
+        }
+
+        /**
+         * Splits entered string by delimiter commands
+         *
+         * @param string $template
+         * @param string $cmdBegin
+         * @param string $cmdEnd
+         * @return array
+         */
+        private static function splitStringByCommand($template, $cmdBegin, $cmdEnd) {
+            $tplPreText = strstr($template, $cmdBegin, true); // Get part before cmdBegin
+            $tplPosTextBegin = strpos($template, $cmdBegin) + strlen($cmdBegin); // Get innerText starting position
+            $tplTextContent = strstr(substr($template, $tplPosTextBegin), $cmdEnd, true); // Get innerText
+            $tplPosTextEnd = strpos($template, $cmdEnd) + strlen($cmdEnd); // Get innerText ending position
+            $tplPostText = substr($template, $tplPosTextEnd); // Get part after innerText
+
+            return [
+                'preText' => $tplPreText,
+                'innerText' => $tplTextContent,
+                'postText' => $tplPostText
+            ];
+        }
+
     }
